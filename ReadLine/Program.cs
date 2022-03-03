3fb6be0173b27bb55ae3f747298e8e317b7256f8
enrique becerra <enriquealejandrobs@hotmail.com>
2022-03-02 21:40:14 -0800

    Agregar archivos de proyecto.

---
+++ ReadLine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            //pregunta el nombre de usuario//
+            Console.WriteLine("Como te llamas?: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine( "hola " +  nombre 
+                + " Quieres ver el menu ");
+            Console.ReadKey();
+        }
+    }
+}
