cc5ebf2b666fc435ff317d600449f8208780da1f
enrique becerra <enriquealejandrobs@hotmail.com>
2022-03-02 21:55:07 -0800

    conversiones

---
+++ Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            string cadena;
+        int num1 = 5, num2, resultado;
+        //preguntamos 
+        }
+    }
+}
---
+++ Conversiones/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversiones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            int num1  = 5, num2 , resultado;
+            //preguntando al usuario el numero//
+            Console.Write("dame un numero para sumarlo: " );
+            //asigna variable de tipo string
+            cadena = Console.ReadLine();
+            num2 = Convert.ToInt32(cadena);
+            //realiza la suma//
+            resultado = num1 + num2;
+            Console.WriteLine("el resultado es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
